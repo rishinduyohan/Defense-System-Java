a8fe99f0e63bcd4865f6eca2f25294f14f3d1876
rishinduyohan <rishinduyohan@gmail.com>
2025-08-10 15:41:22 +0530

    Initial Commit

---
+++ src/ds/view/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package ds.view;
+
+/**
+ *
+ * @author acer
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        new HelicopterForm().setVisible(true);
+    }
+}
